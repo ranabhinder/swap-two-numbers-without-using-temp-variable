a22b38365eaf3e1232a103215d38997f611b31f3
ranabhinder1321 <ranabhinder@yahoo.co.nz>
2021-06-11 17:16:44 +1200

    Add project files.

---
+++ swap two numbers without using temp variable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace swap_two_numbers_without_using_temp_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7, b = 9;
+            Console.WriteLine("value of a is :" +a);
+            Console.WriteLine("value of b is :" +b);
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("\n .......After Swapping....\n");
+            Console.WriteLine("New value of a is :" +a);
+            Console.WriteLine("New value of b is :" + b);
+
+            Console.ReadLine();
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
